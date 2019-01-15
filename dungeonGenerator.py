55af268255e002eb5baa7746ea648b8606ce0228
Simon Heppner <hasibandi@gmx.at>
2019-01-15 17:50:25 +0100

    Update dungeonGenerator.py

--- dungeonGenerator.py
+++ dungeonGenerator.py
@@ -14,10 +14,10 @@ def start():
                     "prob": 0.2,
                     "max": 30,
                     "character": "#"},              
-              "d": {"name": "door of teleportation",
-                    "prob": 0.001,
-                    "max": 2,
-                    "character": "d"},
+              "s": {"name": "strong rock",
+                    "prob": 0.4,
+                    "max": None,
+                    "character": "s"},
               "<": {"name": "stair down",
                     "prob": 0.05,
                     "max": None,
@@ -41,19 +41,19 @@ def start():
     maxpebbles = legend["."]["max"]
     maxbushes = legend["b"]["max"]
     maxrocks = legend["#"]["max"]
-    maxdoors = legend["d"]["max"]
+    maxsrocks = legend["s"]["max"]
     maxstairsdown = legend["<"]["max"]
 
     pebble_prob = legend["."]["prob"]
     bush_prob = legend["b"]["prob"]
     rock_prob = legend["#"]["prob"]
-    door_prob = legend["d"]["prob"]
+    srock_prob = legend["s"]["prob"]
     stairdown_prob = legend["<"]["prob"]
 
     pebble_character = legend["."]["character"]
     bush_character = legend["b"]["character"]
     rock_character = legend["#"]["character"]
-    door_character = legend["d"]["character"]
+    srock_character = legend["s"]["character"]
     stairdown_character = legend["<"]["character"]
     player_character = legend["@"]["character"]
 
@@ -62,7 +62,7 @@ def start():
         #if line == 0 or line == 1 or line == 2 or line == 3 or line == 4 or line == 5 or line == 6 or line == 7 or line == 8 or line == 9:
         for x, char in enumerate(range(maxchars)):
             if x == 0 or x == maxchars-1 or y == 0 or y == maxlines-1:
-                l.append("#")
+                l.append("s")
             #l.append(random.choice(list(legend.keys())))
             else:
                 l.append(random.choice(("#")))
@@ -71,17 +71,18 @@ def start():
     # ---- snake ----
 
     y = random.randint(1, maxlines-1)
-    tp = y
     for x in range(1, maxchars-1):
         d[y][x] = "."
-        if random.random() > 0.5:
+        z = random.random()
+        if z < 0.5:
             # i want to go deeper
             howmuch = random.randint(1, 7)
             if y + howmuch < maxlines-2:
                 for _ in range(howmuch):
                     y += 1
                     d[y][x] = "."
-        elif random.random() < 0.5:
+        #elif z < 0.5:
+        else:
             # i want to go higher
             howmuch = random.randint(1, 7)
             if y - howmuch > 1:
@@ -89,7 +90,7 @@ def start():
                     y -= 1
                     d[y][x] = "."
         # ---- new room? ----
-        elif rt == 1:
+        if rt == 1:
             rt = 0
             room_nr += 1
             maxbreite = maxchars-2 - x
@@ -183,6 +184,13 @@ def start():
             if d[y][x] == ".":
                 pebbles.append((x,y))
     max_pebbles = len(pebbles)
+    
+    walls = []
+    for y, line in enumerate(d):
+        for x, char in enumerate(line):
+            if d[y][x] == "#":
+                walls.append((x, y))
+    max_walls = len(walls)
 
     # buschschleife
 
@@ -207,7 +215,14 @@ def start():
         y = pebbles[n][1]
         if random.random() < stairdown_prob:
             d[y][x] = "<"
-            
+    
+    random.shuffle(walls)
+    for n in range(0, random.randint(1, max_walls//4)):
+        x = walls[n][0]
+        y = walls[n][1]
+        if random.random() < srock_prob:
+            d[y][x] = "s"
+    
     # player generiert
 
     random.shuffle(pebbles)
